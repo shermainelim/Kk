c40fac7717b944a67b96feac778d0ed06181749b
shermainelim <shermaine.lim.si.hui@gmail.com>
2023-03-26 17:18:17 +0800

    cleanup

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import store from "./redux/store";
+
 
 import AppNavigator from "./components/AppNavigator";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
+  
+  
     <AppNavigator />
-  </Provider>
+
+    
 );
 
 // If you want to start measuring performance in your app, pass a function
