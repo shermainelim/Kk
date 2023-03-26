f1d6e8ca1cb7776dcc9ebe970983259872e92f46
shermainelim <shermaine.lim.si.hui@gmail.com>
2023-03-26 16:59:09 +0800

    Update Blogs.jsx

--- src/screens/blogs/Blogs.jsx
+++ src/screens/blogs/Blogs.jsx
@@ -7,51 +7,66 @@ import CustomButton from "../../shared/CustomButton";
 import { useNavigate } from "react-router-dom";
 
 const Blogs = () => {
-  
   const cx = classNames.bind(styles);
 
   const navigate = useNavigate();
 
-  
-
   return (
     <div className={cx("container")}>
-       <img
-            style={{ width: "220px", height: "120px" }}
-            src={Cover}
-            alt="Company Logo"
-          ></img>
-    <div className={cx("blogs")}>
-        CivilTEKK
-      </div>
+      <img
+        style={{ width: "220px", height: "120px" }}
+        src={Cover}
+        alt="Company Logo"
+      ></img>
+      <div className={cx("blogs")}>CivilTEKK</div>
 
       <div className={cx("blogs-desc")}>
-        CivilTekk was created to share my ideas and projects that I feel <br/>
-        will benefit the average engineer. Be it in Civil Engineering, CAD <br/>
-        , Automation or Software Engineering, anything goes inside CivilTekk. <br/>
-        Heck, you can even share what kind of ideas, <br/>
-        if I think is feasible, I will try it out and let you know. 
+        CivilTekk was created to share my ideas and projects that I feel <br />
+        will benefit the average engineer. Be it in Civil Engineering, CAD{" "}
+        <br />
+        , Automation or Software Engineering, anything goes inside CivilTekk.{" "}
+        <br />
+        Heck, you can even share what kind of ideas, <br />
+        if I think is feasible, I will try it out and let you know.
       </div>
       <CustomButton
-            className="civil-blog-btn"
-            content="Come have a look at my blog"
-            clicked={() => {navigate("/")}}
-          ></CustomButton>
+        className="civil-blog-btn"
+        content="Come have a look at my blog"
+        clicked={() => {
+          navigate("/");
+        }}
+      ></CustomButton>
 
+      <div className={cx("blogs")}>NextTEKK</div>
+
+      <div className={cx("blogs-desc")}>
+        NextTEKK was created to provide consulting services
+        <br />
+        for Civil Engineering and Website Consulting Services. <br />
+      </div>
+      <CustomButton
+        className="civil-blog-btn"
+        content="NEXTTEKK"
+        clicked={() => {
+          window.location.href = "https://www.nexttekk.com/";
+        }}
+      ></CustomButton>
 
 <div className={cx("blogs")}>
-       NextTEKK
+       TEKKX
       </div>
 
       <div className={cx("blogs-desc")}>
-      NextTEKK was created to provide consulting services<br/>
-        for Civil Engineering and Website Consulting Services. <br/>
+      Yu-Gi-Oh! has always been a hobby, hence I created this site <br/>
+      to sell my second hand products on the web. <br/>
       </div>
       <CustomButton
             className="civil-blog-btn"
-            content="NEXTTEKK"
-            clicked={() => {window.location.href = 'https://www.nexttekk.com/'}}
+            content="TEKKX"
+            clicked={() => {window.location.href = 'https://tekkx.com/'}}
           ></CustomButton>
+  
+
     </div>
   );
 };
